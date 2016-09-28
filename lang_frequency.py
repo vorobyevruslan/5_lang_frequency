42831cdc7c0fd7715ce7abc4d9de23d000630edf
vorobyevruslan <bodyrof1@gmail.com>
2016-09-28 22:23:52 +0600

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
-
-
 def load_data(filepath):
-    pass
-
+    a=[]
+    with open(filepath) as iner:
+        for line in iner:
+            a+=line.split()
+    return a
 
 def get_most_frequent_words(text):
-    pass
-
+    d={}
+    for i in text:
+        if i in d:
+            d[i]+=1
+        else:
+            d[i]=1
+    return sorted(d, key=d.__getitem__)
 
 if __name__ == '__main__':
-    pass
+    filepath1=input()
+    text=load_data(filepath1)
+    d=get_most_frequent_words(text)
+    for i in range(len(d)):
+        print(d[-i-1])
+        if i==9:
+            break
